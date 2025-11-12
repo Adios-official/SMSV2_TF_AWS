4d8951f678dd15595ef1a30e52e5278e19b4a4cc
Adityakiran <a.adityakiran@f5.com>
2025-11-12 20:44:38 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,13 @@
-#########################################################################################################
-# OUTPUT OF PUBLIC IPs Allocated to SLO
-#########################################################################################################
+################################################################################
+# 1. ORIGINAL OUTPUT (FOR PUBLIC IPS)
+################################################################################
+
 output "allocated_public_ips_to_SLO" {
   description = "List of public IPs allocated to the instances. Will be empty if public_ip_mode is 'NONE'."
   
-  # This value expression is now a nested conditional:
-  # 1. If "CREATE_EIP", get IPs from the 'aws_eip' resource.
-  # 2. Else if "USE_EXISTING_EIP", get IPs from the 'data.aws_eip.lookup'.
-  # 3. Else ("NONE"), return an empty list [].
-  value = var.public_ip_mode == "CREATE_EIP" ? [for eip in aws_eip.example : eip.public_ip] : (
-            var.public_ip_mode == "USE_EXISTING_EIP" ? [for alloc_id in var.existing_eip_allocation_ids : data.aws_eip.lookup[alloc_id].public_ip] : []
+  # This value expression includes the 'tolist()' fix to clean up the terminal display
+  value = var.public_ip_mode == "CREATE_EIP" ? tolist([for eip in aws_eip.example : eip.public_ip]) : (
+            var.public_ip_mode == "USE_EXISTING_EIP" ? tolist([for alloc_id in var.existing_eip_allocation_ids : data.aws_eip.lookup[alloc_id].public_ip]) : []
           )
 }
 
@@ -20,4 +18,43 @@ data "aws_eip" "lookup" {
 
   # Look up the EIP by its allocation_id, which is passed to the 'id' argument
   id = each.value
+}
+
+################################################################################
+# 2. NEW DEPLOYMENT SUMMARY OUTPUT
+################################################################################
+
+output "deployment_summary" {
+  description = "A structured summary of all deployed resources and their key information."
+  
+  # Display logic
+  value = {
+    # 1. Summary of inputs
+    deployment_model = var.deployment_model
+    cluster_name     = var.cluster_name
+    node_count       = var.num_nodes
+    nic_count        = var.num_nics
+    public_ip_mode   = var.public_ip_mode
+    aws_region       = var.region
+
+    # 2. AWS Compute outputs
+    aws_instance_ids = [for instance in aws_instance.ec2_instance : instance.id]
+    aws_instance_azs = [for instance in aws_instance.ec2_instance : instance.availability_zone]
+
+    # 3. AWS Networking outputs
+    public_ips = var.public_ip_mode == "CREATE_EIP" ? tolist([for eip in aws_eip.example : eip.public_ip]) : (
+                   var.public_ip_mode == "USE_EXISTING_EIP" ? tolist([for alloc_id in var.existing_eip_allocation_ids : data.aws_eip.lookup[alloc_id].public_ip]) : []
+                 )
+    private_ips = [for nic in aws_network_interface.slo_nics : nic.private_ip]
+    created_security_groups = {
+      slo = try(aws_security_group.slo_sg[0].id, null)
+      sli = try(aws_security_group.sli_sg[0].id, null)
+    }
+
+    # 4. F5 XC outputs
+    f5_xc_site_names = [for site in volterra_securemesh_site_v2.smsv2-site-object : site.name]
+    f5_xc_virtual_site_name = (
+      var.deployment_model == "vsite" ? try(volterra_virtual_site.smsv2-vsite[0].name, null) : "N/A (Cluster Model)"
+    )
+  }
 }
